df00dda7ee214f0ec1c7b5e39909527c927d597d
A先生 <142523398+axs66@users.noreply.github.com>
2025-09-20 17:57:56 +0800

    Update CS2BackGestureSettingsViewController.h

--- Controllers/CS2BackGestureSettingsViewController.h
+++ Controllers/CS2BackGestureSettingsViewController.h
@@ -1,7 +1,10 @@
 #import <UIKit/UIKit.h>
 
+// 前向声明，避免重复导入
+@class CSSettingSection;
+
 @interface CS2BackGestureSettingsViewController : UITableViewController
 
-@property (nonatomic, strong) NSArray *sections;
+@property (nonatomic, strong) NSArray<CSSettingSection *> *sections;
 
 @end
